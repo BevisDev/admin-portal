b9dee9d9d8577eedefe2e92014374f27102b0871
BevisDev <dev.binhtt@gmail.com>
2026-04-04 20:15:50 +0700

    del login.ts

--- src/models/login.ts
+++
@@ -1,9 +0,0 @@
-import type { NavigateFunction } from "react-router-dom";
-import type { Me } from "../types/auth/Me";
-
-type Login = {
-  from: string;
-  me: Me | null;
-  setMe: (data: Me) => void;
-  navigate: NavigateFunction;
-};
